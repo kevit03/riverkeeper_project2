6e8ff7e0ae2b9a2ff0d2cdda1b6d2e62f7975a9c
Isabella Wrobleski <imw2298@nyu.edu>
2025-10-19 11:02:15 -0400

    add functions, make df

--- scripts/Izzy/core-analysis.py
+++ scripts/Izzy/core-analysis.py
@@ -1,75 +1,108 @@
 import pandas as pd
-import numpy as np
 from decimal import Decimal, getcontext
-from scipy import stats
 import calendar
 
 getcontext().prec = 32
 
 df = pd.read_csv("scripts/Izzy/cleaned.csv")
+df["last_gift_date"] = pd.to_datetime(df["last_gift_date"]) # transforming last gift date column to datetime
 
-# total amount of donations
-# using decimal module to make sure amount of cents is precise
-donations_precise = [Decimal(str(df.loc[row]["total_gifts_amount"])) for row in df.index]
-donation_total = np.sum(donations_precise)
-print(f"Total Donations: ${donation_total:,f}")
+def donations_precise(df: pd.DataFrame) -> pd.Series:
+    '''
+    converts the dollar amounts to Decimal objects to maintain precision
+    '''
+    precise = df["total_gifts_amount"].apply(lambda x: Decimal(str(x)))
+    return precise
 
-# average donation 
-donation_avg = np.mean(donations_precise)
-print(f"Average Donation: ${donation_avg:,.2f}")
+def total_donations(df: pd.DataFrame) -> float:
+    '''
+    returns the sum of all donations by all donors
+    '''
+    donation_total = donations_precise(df).sum()
+    return round(donation_total, 2)
 
-# median donation
-# computing since average is likely skewed by large donations
-donation_med = np.median(donations_precise)
-print(f"Median Donation: ${donation_med:,.2f}")
+def avg_total_donation(df: pd.DataFrame) -> float:
+    '''
+    returns the average total donation amount
+    '''
+    donation_avg = donations_precise(df).mean()
+    return round(donation_avg, 2)
 
-# mode donation
-donation_mode = df["total_gifts_amount"].value_counts().index[0]
-print(f"Modal Donation: ${donation_mode:,.2f}")
+def median_total_donation(df: pd.DataFrame) -> float:
+    '''
+    returns the median total donation amount
+    '''
+    donation_median = donations_precise(df).median()
+    return round(donation_median, 2)
 
-# unique donors
-unique_donors = len(df["account_id"].unique())
-print(f"Unique Donors: {unique_donors:,d}")
+def modal_total_donation(df: pd.DataFrame) -> float:
+    '''
+    returns the most common total donation amount
+    '''
+    donation_mode = donations_precise(df).value_counts().index[0]
+    return round(donation_mode, 2)
 
-# categorizing donors
-# using categories based on riverkeeper's website:
-    # donors of $20 or more receive email newsletters, invitations, etc.
-    # donors of $50 or more receive the "opportunity to vote on [the] Board of Directors at an annual Membership meeting"
-    # donors of $250 or more "are listed in [the] annual Impact Report"
-df.loc[(df["total_gifts_amount"] < 20), "category"] = "under 20"
-df.loc[(df["total_gifts_amount"] >= 20) & (df["total_gifts_amount"] < 50), "category"] = "20+"
-df.loc[(df["total_gifts_amount"] >= 50) & (df["total_gifts_amount"] < 250), "category"] = "50+"
-df.loc[df["total_gifts_amount"] >= 250, "category"] = "250+"
-print("\nDonor Categories:")
-for category, count in df["category"].value_counts().items():
-    print(f"{category}: {count:,d}")
+def unique_donors(df: pd.DataFrame) -> int:
+    '''
+    returns the number of unique donors
+    '''
+    return len(df["account_id"].unique())
 
-# top donors
-top_donors = df["total_gifts_amount"].sort_values(ascending=False)[:10]
-print("\nTop Donors:")
-for index, amt in top_donors.items():
-    id = df.loc[index]["account_id"]
-    print(f"{id}: ${amt:,.2f}")
+def categorize_donors(df: pd.DataFrame) -> pd.Series:
+    '''
+    adds a column to the dataset categorizing donors based on amount
+    returns value counts for categories
+    categories come from riverkeeper's website: https://engage.riverkeeper.org/give:
+        donors of $20 or more receive email newsletters, invitations, etc.
+        donors of $50 or more receive the opportunity to vote on the Board of Directors at an annual Membership meeting
+        donors of $250 or more are listed in the annual Impact Report
+    '''
+    df.loc[(df["total_gifts_amount"] < 20), "category"] = "under 20"
+    df.loc[(df["total_gifts_amount"] >= 20) & (df["total_gifts_amount"] < 50), "category"] = "20+"
+    df.loc[(df["total_gifts_amount"] >= 50) & (df["total_gifts_amount"] < 250), "category"] = "50+"
+    df.loc[df["total_gifts_amount"] >= 250, "category"] = "250+"
+    return df["category"].value_counts()
 
-# grouping by month/year
-df["last_gift_date"] = pd.to_datetime(df["last_gift_date"])
+def top_donors(df: pd.DataFrame, n: int) -> pd.DataFrame:
+    '''
+    returns top n donors with account id, total amount, and date of last donation
+    '''
+    sorted_donations = df.sort_values(by="total_gifts_amount", ascending=False)
+    top_donors = sorted_donations[:n]
+    return top_donors[["account_id", "total_gifts_amount", "last_gift_date"]]
 
-# getting counts per month, changing index to month names
-month_counts = df["last_gift_date"].dt.month.value_counts().sort_index()
-month_counts.index = calendar.month_abbr[1:]
+def month_counts(df: pd.DataFrame) -> pd.Series:
+    '''
+    returns series with the number of donors who made their last donation in each month
+    '''
+    month_counts = df["last_gift_date"].dt.month.value_counts().sort_index()
+    month_counts.index = calendar.month_abbr[1:]
+    return month_counts
+    
+def year_counts(df: pd.DataFrame) -> pd.Series:
+    '''
+    returns series with the number of donors who made their last donation in each year
+    '''
+    year_counts = df["last_gift_date"].dt.year.value_counts().sort_index()
+    return year_counts
+    
+def data_per_year(df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    returns a dataframe with columns:
+        year
+        number of donors who last donated in that year
+        top donor who last donated in that year
+        average total donation amount of those who stopped donating that year
+        median total donation amount of those who stopped donating that year
+    '''
+    res = pd.DataFrame(index=year_counts(df).index)
+    res.index.name = "Year"
 
-# printing month, counts, and total amount of donations in that month
-# sorted by number of donations
-print("\nDonations per Month:")
-for month, count in month_counts.sort_values(ascending=False).items():
-    month_index = list(calendar.month_abbr).index(month)
-    amt = sum(df["total_gifts_amount"][df["last_gift_date"].dt.month==month_index])
-    print(f"{month}: {count:,d} (${amt:,.2f})")
+    res["Count"] = year_counts(df).values
+    res["Top Amount"] = df.groupby(df["last_gift_date"].dt.year)["total_gifts_amount"].max()
+    res["Average Amount"] = df.groupby(df["last_gift_date"].dt.year)["total_gifts_amount"].mean().round(2)
+    res["Median Amount"] = df.groupby(df["last_gift_date"].dt.year)["total_gifts_amount"].median()
 
-# getting counts per year, printing year, counts, and total amount of donations in that year
-# sorted by number of donations
-year_counts = df["last_gift_date"].dt.year.value_counts().sort_values(ascending=False)
-print("\nDonations per Year:")
-for year, count in year_counts.items():
-    amt = sum(df["total_gifts_amount"][df["last_gift_date"].dt.year==year])
-    print(f"{year}: {count:,d} (${amt:,.2f})")
+    return res
+
+print(data_per_year(df))
