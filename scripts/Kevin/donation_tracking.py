c362087bcadddb973b3bcb9c678b8b53bb0b6bb3
cryptonickevit <74779238+cryptonickevit@users.noreply.github.com>
2025-10-31 15:05:05 -0400

    I fixed the root for the donation track and cached more.

--- scripts/Kevin/donation_tracking.py
+++ scripts/Kevin/donation_tracking.py
@@ -3,15 +3,15 @@ from pathlib import Path
 import pandas as pd
  
 repo_root = Path(__file__).resolve().parents[2] #creates a root relative to this file 
-csv_path = repo_root / 'scripts' / 'Izzy' / 'cleaned.csv' #accesses cleaned.csv using Path "/"
+csv_path = repo_root / 'data' / 'Riverkeeper_Donors_for_NYU_Biokind_Project-10.22.25.csv' #accesses cleaned.csv using Path "/"
 
-data = pd.read_csv(csv_path, parse_dates=['last_gift_date'])
+data = pd.read_csv(csv_path, parse_dates=['Last Gift Date'])
 
-data['Year'] = data['last_gift_date'].dt.year #extracts the year 
-data['Month'] = data['last_gift_date'].dt.month #extracts the month 
+data['Year'] = data['Last Gift Date'].dt.year #extracts the year 
+data['Month'] = data['Last Gift Date'].dt.month #extracts the month 
 
 monthly_donations = ( 
-    data.groupby(['Year', 'Month'])['total_gifts_amount']
+    data.groupby(['Year', 'Month'])['Total Gifts (All Time)']
     .sum() #sums up the total by year + month 
     .reset_index() #assigns a new index and turns the old one into a column
 )
@@ -32,11 +32,11 @@ def pie_maker(size1, size2, values, labels, autopct, startangle, colors, shadow,
 df = pd.DataFrame(monthly_donations) #turned monthly donations into dataframe
 pf = pd.DataFrame(data) #turned the parsed data into a dataframe
 
-max_value = monthly_donations['total_gifts_amount'].max() #found the max amount donated
+max_value = monthly_donations['Total Gifts (All Time)'].max() #found the max amount donated
 
 #bar chart
 for year, group in monthly_donations.groupby('Year'): #opens a new chart window for each year 
-    plt.bar(group['Month'], group['total_gifts_amount'])
+    plt.bar(group['Month'], group['Total Gifts (All Time)'])
     plt.title(f'Amount of Money Donated by Month in {year}')
     plt.xlabel('Month')
     plt.ylabel('Money ($)')
@@ -48,10 +48,10 @@ colors = ['#ff9999','#66b3ff','#99ff99','#ffcc99']
 
 bins = [1, 250, 5000, 100000, float('inf')]
 labels = ["Small ($1-250)", "Medium ($250-5k)", "Large ($5k-$100k)", "Major ($100k+)" ]
-pf['donation_range'] = pd.cut(pf['total_gifts_amount'], bins = bins, labels = labels, right = False)
+pf['donation_range'] = pd.cut(pf['Total Gifts (All Time)'], bins = bins, labels = labels, right = False)
 
 donation_summary = (
-    pf.groupby('donation_range')['total_gifts_amount']
+    pf.groupby('donation_range')['Total Gifts (All Time)']
     .sum()
     .reindex(labels)
 )#sums up the money by donation range 
